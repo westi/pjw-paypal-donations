106e8b44fa71930c4a370777e55c639ed4b37d6a
Peter Westwood <peter.westwood@ftwr.co.uk>
2015-08-25 17:29:54 +0000

    Add a first pass set of custom columns for the admin ui.

--- paypal-donations.php
+++ paypal-donations.php
@@ -23,6 +23,8 @@ class pjw_paypal_donation_manager {
 		add_action( 'pjw_ipn_verified_for-web_accept', array( $this, 'ipn_received' ) );
 		add_action( 'pjw_ppdm_donation_received', array( $this, 'donation_received' ) );
 		add_action( 'init', array( $this, 'register_donation_post_type' ) );
+		add_filter( 'manage_pjw-donation_posts_columns', array( $this, 'register_custom_post_type_columns' ) );
+		add_action( 'manage_pjw-donation_posts_custom_column', array( $this, 'display_custom_post_type_columns' ), 10, 2 );
 	}
 
 	private function debug_log( $thing ) {
@@ -115,6 +117,36 @@ class pjw_paypal_donation_manager {
 				'map_meta_cap' => true,
 			)
 		);
+	}
+
+	/**
+	 * Modify the list of columns that are displayed for posts in wp-admin.
+	 * 
+	 * @param array $_columns
+	 * @return array
+	 */
+	public function register_custom_post_type_columns( $_columns ) {
+		unset( $_columns['date'] ); // Remove the default date position so we can rename and add at the end.
+		$_columns['pjw_ppdm-campaign'] = 'Campaign';
+		$_columns['pjw_ppdm-txn_id'] = 'Transaction ID';
+		$_columns['pjw_ppdm-email'] = 'Donor Email';
+		$_columns['pjw_ppdm-amount'] = 'Amount';
+		$_columns['date'] = 'Donation Date';
+		return $_columns;
+	}
+
+	/**
+	 * Output the content for the custom columns in wp-admin
+	 */
+	public function display_custom_post_type_columns( $_column_name, $_post_id ) {
+		switch( $_column_name ) {
+			case 'pjw_ppdm-campaign':
+			case 'pjw_ppdm-txn_id':
+			case 'pjw_ppdm-email':
+			case 'pjw_ppdm-amount':
+				echo get_post_meta( $_post_id, $_column_name, true );
+				break;
+		}
 	}
 
 	/**
