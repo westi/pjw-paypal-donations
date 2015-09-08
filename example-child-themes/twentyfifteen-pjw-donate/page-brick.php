973aa8840ce8b51cce260b0e8a29b6427cfa5c80
Peter Westwood <peter.westwood@ftwr.co.uk>
2015-09-08 19:25:30 +0000

    Switch to $20 bricks.

--- example-child-themes/twentyfifteen-pjw-donate/page-brick.php
+++ example-child-themes/twentyfifteen-pjw-donate/page-brick.php
@@ -10,18 +10,19 @@ get_header(); ?>
 
 			<div id='brick-wall-frame'>
 			<img src="http://i1.wp.com/blog.ftwr.co.uk/wp-content/uploads/2015/07/IMG_2941.jpg?resize=466%2C466" id="hidden-behind-the-wall" />
-				<?php // 20 * 2 * 14 = 560 => $11,200
+				<?php // 20 * 2 * 14 = 560 => $11,200 Max
 					// Consistently random
 					mt_srand( 12 );
 
 					$_donated_amt = $pjw_pdm->get_total_donations( '2015-appeal' );
 					$_donated = array();
+
 					// Build an array of donated bricks randomly across the whole wall based on the donations so far
 					while( $_donated_amt > 0 ) {
 						$_pos = mt_rand( 1, 560 );
 						if ( ! isset( $_donated[$_pos] ) ) {
 							$_donated[$_pos] = true;
-							$_donated_amt -= 1; // TODO make this $20
+							$_donated_amt -= 20;
 						}
 					}
 
