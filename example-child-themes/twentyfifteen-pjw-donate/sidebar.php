ba263e4f7dbc939f1cb513d55a8eeae90ce28db7
Peter Westwood <peter.westwood@ftwr.co.uk>
2015-09-08 20:43:05 +0000

    Add some sandboxed donation buttons for $20 and $200

--- example-child-themes/twentyfifteen-pjw-donate/sidebar.php
+++ example-child-themes/twentyfifteen-pjw-donate/sidebar.php
@@ -13,6 +13,23 @@ if ( is_page_template( 'page-brick.php' ) ) : ?>
 		<?php if ( is_active_sidebar( 'sidebar-brick' ) ) : ?>
 			<div id="widget-area" class="widget-area" role="complementary">
 				<?php dynamic_sidebar( 'sidebar-brick' ); ?>
+				<aside class="widget">
+					$20
+					<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
+						<input type="hidden" name="cmd" value="_s-xclick">
+						<input type="hidden" name="hosted_button_id" value="K423NKBN7GLQQ">
+						<input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+						<img alt="" border="0" src="https://www.sandbox.paypal.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+					</form>
+					<br/>
+					$200
+					<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
+						<input type="hidden" name="cmd" value="_s-xclick">
+						<input type="hidden" name="hosted_button_id" value="AMSYYM97X2L5Q">
+						<input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+						<img alt="" border="0" src="https://www.sandbox.paypal.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+					</form>
+				</aside>
 			</div><!-- .widget-area -->
 		<?php endif; ?>
 
