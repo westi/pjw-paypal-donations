744cc84fb8c7b8830638c2c6a8a4b92f66679ba8
Peter Westwood <peter.westwood@ftwr.co.uk>
2015-10-27 18:23:42 +0000

    Don't process non-post requests in the IPN Handler.

--- includes/ipn-handler.php
+++ includes/ipn-handler.php
@@ -43,31 +43,32 @@ class pjw_ipn_handler {
 	}
 
 	public function action_parse_request() {
-		if ( isset( $GLOBALS['wp']->query_vars[ '_pjw_ipn_handler' ] ) ) {
-
-			header('HTTP/1.1 200 OK');
-			$this->debug_log( 'IPN Received' );
-			$_to_verfiy = array( 'cmd' => '_notify-validate' );
-			$_to_verify = $_to_verfiy + $_POST;
-			$response = wp_remote_post(
-				( $this->sandbox ? 'https://www.sandbox.paypal.com/cgi-bin/webscr' : 'https://www.paypal.com/cgi-bin/webscr' ),
-				array(
-					'body' => $_to_verify,
-					'useragent' => 'PJW IPN Handler/1.0'
-				)
-			);
-			$_response = wp_remote_retrieve_body( $response );
-			switch( $_response ) {
-					case 'VERIFIED':
-						$this->debug_log( 'IPN VERIFIED for ' . $_POST['txn_type'] );
-						do_action( 'pjw_ipn_verified_for-' . $_POST['txn_type'], $_POST );
-						break;
-					case 'INVALID':
-						$this->debug_log( 'IPN INVALID for ' . $_POST['txn_type'] );
-						do_action( 'pjw_ipn_invalid_for-' . $_POST['txn_type'], $_POST );
-						break;
+		if ( ! empty( $_POST ) ) {
+			if ( isset( $GLOBALS['wp']->query_vars[ '_pjw_ipn_handler' ] ) ) {
+				header('HTTP/1.1 200 OK');
+				$this->debug_log( 'IPN Received' );
+				$_to_verfiy = array( 'cmd' => '_notify-validate' );
+				$_to_verify = $_to_verfiy + $_POST;
+				$response = wp_remote_post(
+					( $this->sandbox ? 'https://www.sandbox.paypal.com/cgi-bin/webscr' : 'https://www.paypal.com/cgi-bin/webscr' ),
+					array(
+						'body' => $_to_verify,
+						'useragent' => 'PJW IPN Handler/1.0'
+					)
+				);
+				$_response = wp_remote_retrieve_body( $response );
+				switch( $_response ) {
+						case 'VERIFIED':
+							$this->debug_log( 'IPN VERIFIED for ' . $_POST['txn_type'] );
+							do_action( 'pjw_ipn_verified_for-' . $_POST['txn_type'], $_POST );
+							break;
+						case 'INVALID':
+							$this->debug_log( 'IPN INVALID for ' . $_POST['txn_type'] );
+							do_action( 'pjw_ipn_invalid_for-' . $_POST['txn_type'], $_POST );
+							break;
+				}
+				exit;
 			}
-			exit;
 		}
 	}
 }
